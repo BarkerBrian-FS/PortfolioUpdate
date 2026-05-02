64414f56d83647e55aea247873bd47290a631c44
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-05-02 12:50:34 -0500

    changed social buttons on contact and found overflow problem

--- src/Components/ContactButtons.js
+++ src/Components/ContactButtons.js
@@ -2,45 +2,40 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import github from '../images/github.png';
 import linkedIn from '../images/linkedin.png';
-import './Contact.css';
+import './ContactButtons.css';
+
 const ContactButtons = () => {
+  const icons = [
+    {
+      href: 'https://github.com/BarkerBrian-FS?tab=repositories',
+      img: github,
+      alt: 'github'
+    },
+    {
+      href: 'https://www.linkedin.com/in/brian-barker-498537202/',
+      img: linkedIn,
+      alt: 'linkedIn'
+    }
+  ];
+
   return (
-    <div>
-        <div style={styles.contact}>
-            <motion.div
-                whileHover={{ scale: 1.1 }} style={styles.github} className='github'>
-                    <a href='https://github.com/BarkerBrian-FS?tab=repositories'><img  style={styles.github} src={github} alt='github'/></a>
-            </motion.div>
-            <motion.div
-                whileHover={{ scale: 1.1 }} className='linkedIn'>
-                    <a href='https://www.linkedin.com/in/brian-barker-498537202/'><img  style={styles.linkedIn} src={linkedIn}  alt='linkedIn'/></a>
-            </motion.div>
-        </div>
+    <div className="socialRow">
+      {icons.map((icon, i) => (
+        <motion.a
+          key={i}
+          href={icon.href}
+          target="_blank"
+          rel="noreferrer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="socialIcon"
+        >
+          <img src={icon.img} alt={icon.alt} />
+        </motion.a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ContactButtons
+export default ContactButtons;
 
-const styles = {
-  github:{
-      height: '105px',
-      width: '105px',
-      marginRight: '10px',
-      backgroundColor: 'white',
-      borderRadius: '20%'
-  },
-  linkedIn:{
-      height: '105px',
-      width: '105px',
-      marginLeft: '10px'
-  },
-  contact:{
-      display: 'flex',
-      flexDirection: 'row',
-      marginLeft: '43%',
-      marginRight: 'auto',
-      marginTop: '40px',
-      justifyContent: 'spaceBetween',
-  }
-}
